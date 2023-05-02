21f85652d80e95a7971651b115bf6aa853141765
guirep <tarek.zemmari@gmail.com>
2023-05-02 21:22:33 +0200

    preparando el deploy

---
+++ src/components/About/About.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import "./About.css";
+import MiFoto from "../../assets/images/Sin-Fondo.png";
+import AOS from "aos";
+
+const About = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
+  return (
+    <div className="about-container" id="about">
+      <div className="contenedor-sobre-mi">
+        <div className="about-desc" data-aos="fade-left">
+          <h3>Bienvenidos a mi portfolio personal </h3>
+          <p>
+            <span>
+              Soy Tarek Zemmari, un apasionado Full Stack Developer  con una amplia experiencia en desarrollo y maquetación de aplicaciones web. Conocido por mi enfoque en la búsqueda de soluciones innovadoras y mi capacidad para entregar proyectos de alta calidad en tiempo y forma.
+            </span>
+            <br />
+            Mi pasión por la programación surgió desde muy joven, por lo que estudié Informática en el bachillerato. Continué mi formación y realicé un bootcamp que completé con éxito, lo que me permitió dedicarme profesionalmente al desarrollo web. Desde entonces, he trabajado con clientes de distintas industrias, entregando soluciones de alta calidad y adaptadas a sus necesidades.
+            <br />
+            Soy experto en el desarrollo web con PHP, tanto puro como con su framework Symfony 5.4. Además, tengo experiencia en la creación de sitios web utilizando HTML y jQuery, y también he trabajado con React en algunos proyectos.
+            <br />
+            Si estás buscando un desarrollador web apasionado y comprometido para tu próximo proyecto, ¡no dudes en contactarme! Me encantaría discutir cómo puedo ayudarte a alcanzar tus objetivos.
+          </p>
+        </div>
+        <div className="about-img" data-aos="fade-left">
+          <img className="imagen-sobre-mi" src={MiFoto} alt="MiFoto" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
