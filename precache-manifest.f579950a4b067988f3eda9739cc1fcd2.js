0e23316562f07af6dc3ba16f6db3ebc36e28a2e9
Tarek <tarek@tedybusiness.com>
2023-05-04 17:07:27 +0200

    Updates

---
+++ precache-manifest.f579950a4b067988f3eda9739cc1fcd2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b16f76aa3b4437b2bcf",
+    "url": "/m_portafolio/static/css/main.df3a95d8.chunk.css"
+  },
+  {
+    "revision": "7b16f76aa3b4437b2bcf",
+    "url": "/m_portafolio/static/js/main.7b16f76a.chunk.js"
+  },
+  {
+    "revision": "5c7d1b126ae4d0bd9124",
+    "url": "/m_portafolio/static/js/1.5c7d1b12.chunk.js"
+  },
+  {
+    "revision": "1bcb5bda384c42ea36d1",
+    "url": "/m_portafolio/static/css/2.c7fd6b4f.chunk.css"
+  },
+  {
+    "revision": "1bcb5bda384c42ea36d1",
+    "url": "/m_portafolio/static/js/2.1bcb5bda.chunk.js"
+  },
+  {
+    "revision": "09a5fd9111b2aaec74d8",
+    "url": "/m_portafolio/static/js/runtime~main.09a5fd91.js"
+  },
+  {
+    "revision": "60eb4491fda2a6aa06eee21abbefca83",
+    "url": "/m_portafolio/static/media/Sin-Fondo.60eb4491.png"
+  },
+  {
+    "revision": "2329e327ecef63d0a8842b22dd201935",
+    "url": "/m_portafolio/static/media/tec-movil.2329e327.png"
+  },
+  {
+    "revision": "5cd39a55f0bed08899466e23c002e986",
+    "url": "/m_portafolio/static/media/react-dent.5cd39a55.png"
+  },
+  {
+    "revision": "7a2fb8661a060430e44f64c56d0c59c7",
+    "url": "/m_portafolio/static/media/net-film.7a2fb866.png"
+  },
+  {
+    "revision": "718f7aa05a109b947cd13a0bdcc97f3d",
+    "url": "/m_portafolio/static/media/git.718f7aa0.png"
+  },
+  {
+    "revision": "dfb61d9189833a07a12957eba4da199e",
+    "url": "/m_portafolio/static/media/javascript.dfb61d91.png"
+  },
+  {
+    "revision": "43d78112faec28f7c5b3bddec9795b2a",
+    "url": "/m_portafolio/static/media/react.43d78112.png"
+  },
+  {
+    "revision": "0d5b39a003afcecd87ccbe375d67631e",
+    "url": "/m_portafolio/static/media/twig.0d5b39a0.png"
+  },
+  {
+    "revision": "4b8b8cedcff35f2f7525109af1a12d3a",
+    "url": "/m_portafolio/static/media/imagen_4.4b8b8ced.png"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/m_portafolio/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/m_portafolio/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "aab73283f839e775f9ac86d642983653",
+    "url": "/m_portafolio/static/media/boxicons.aab73283.woff2"
+  },
+  {
+    "revision": "3a0cb82447f8e9ce865a709a92f0c752",
+    "url": "/m_portafolio/static/media/boxicons.3a0cb824.woff"
+  },
+  {
+    "revision": "4002d1c83d8dd40df85708c5601993e4",
+    "url": "/m_portafolio/static/media/boxicons.4002d1c8.eot"
+  },
+  {
+    "revision": "878061312a371566f591f1f1a9f76a87",
+    "url": "/m_portafolio/static/media/boxicons.87806131.ttf"
+  },
+  {
+    "revision": "0da09c31aacfc19b901ae6779e23aeb9",
+    "url": "/m_portafolio/static/media/boxicons.0da09c31.svg"
+  },
+  {
+    "revision": "a1c51c7639eeb9fbc8ca7656add75c80",
+    "url": "/m_portafolio/static/media/hero-bg.a1c51c76.jpg"
+  },
+  {
+    "revision": "b1bba3b7d70e13102f435e48cc22337e",
+    "url": "/m_portafolio/index.html"
+  }
+];
